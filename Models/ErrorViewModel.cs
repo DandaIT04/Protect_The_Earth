6c40acf5030a9cbfea558f38438a339b878d41c4
KeaneDT <S10202630@connect.np.edu.sg>
2021-10-27 13:22:30 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PFD_SaveTheEnvironment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
