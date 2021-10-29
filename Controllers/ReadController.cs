ec9dce829d46b92289286181391c16b9edb6b3ab
KeaneDT <S10202630@connect.np.edu.sg>
2021-10-29 10:33:10 +0800

    Added Uncompleted Skeleton

---
+++ Controllers/ReadController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_SaveTheEnvironment.Controllers
+{
+    public class ReadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Organizations()
+        {
+            return View();
+        }
+    }
+}
