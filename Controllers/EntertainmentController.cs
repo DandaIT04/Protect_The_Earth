118362f0114e14be77fd1964ffb2f11aa53079c1
Muhd Dandarawie Bin Rohani <s10208266@connect.np.edu.sg>
2021-10-30 16:42:37 +0800

    Added games tab. Added earth floating game (testing. Not confirmed)

---
+++ Controllers/EntertainmentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_SaveTheEnvironment.Controllers
+{
+    public class EntertainmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Games()
+        {
+            return View();
+        }
+    }
+}
