3cb9124091aa8325bf63a682e3501a96fc17fd50
KeaneDT <S10202630@connect.np.edu.sg>
2021-10-31 15:31:47 +0800

    Included PolyEarth Database setup script/Model for Users/Package for SQL

---
+++ Models/Users.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PFD_SaveTheEnvironment.Models
+{
+    public class Users
+    {
+        public int UserID { get; set; }
+
+        //UserName
+        [Required(ErrorMessage = "User Name Required!")]
+        [Display(Name = "User Name")]
+        [StringLength(50, ErrorMessage = "User Name cannot be more than 50 characters!")]
+        public string UserName { get; set; }
+
+        //Salutation
+        [StringLength(5)]
+        public string Salutation { get; set; } //Nullable - from db
+
+
+        //EmailAddr
+        [Required(ErrorMessage = "Email Address Required!")]
+        [Display(Name = "Email Address")]
+        [EmailAddress]
+        [StringLength(50, ErrorMessage = "Email length cannot be more than 50 characters!")]
+        public string EmailAddr { get; set; }
+
+        //Password
+        //Default value set
+        [Required(ErrorMessage = "Password Required!")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(255, ErrorMessage = "Password must be atleast 6 characters long!", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        //Score
+        public int Score { get; set; }
+
+    }
+}
---
+++ PolyEarth_DB_SetUp_Script.sql
@@ -0,0 +1,46 @@
+Create Database PolyEarth_DB
+GO
+
+Use PolyEarth_DB
+GO
+
+/***************************************************************/
+/***           Delete tables before creating                 ***/
+/***************************************************************/
+
+/* Table: dbo.Users */
+if exists (select * from sysobjects 
+  where id = object_id('dbo.Users') and sysstat & 0xf = 3)
+  drop table dbo.Users
+GO
+
+/***************************************************************/
+/***                     Creating tables                     ***/
+/***************************************************************/
+
+/* Table: dbo.User */
+CREATE TABLE dbo.Users
+(
+  UserID 			int IDENTITY (1,1),
+  UserName		varchar(50) 	NOT NULL,
+  Salutation			varchar(5)  	NULL	
+                                        CHECK (Salutation IN ('Dr','Mr','Ms','Mrs','Mdm')),
+  EmailAddr		    	varchar(50)  	NOT NULL,
+  [Password]		    varchar(255)  	NOT NULL DEFAULT ('password123'),
+  Score		varchar(50) 	NOT NULL DEFAULT ('0'),
+  CONSTRAINT PK_Users PRIMARY KEY NONCLUSTERED (UserID)
+)
+GO
+
+/***************************************************************/
+/***                Populate Sample Data                     ***/
+/***************************************************************/
+
+SET IDENTITY_INSERT [dbo].[Users] ON 
+INSERT [dbo].[Users] ([UserID], [UserName], [Salutation], [EmailAddr], [Password], [Score]) 
+VALUES (1, 'John Doe', 'Mr', 'jd1@hotmail.com', 'password123', '0')
+INSERT [dbo].[Users] ([UserID], [UserName], [Salutation], [EmailAddr], [Password], [Score]) 
+VALUES (2, 'Casca Susan', 'Mrs', 'cs1@gmail.com', 'password123', '100')
+INSERT [dbo].[Users] ([UserID], [UserName], [Salutation], [EmailAddr], [Password], [Score]) 
+VALUES (3, 'Gon Yeager', 'Dr', 'gy1@hotmail.com', 'password123', '999')
+SET IDENTITY_INSERT [dbo].[Users] OFF
