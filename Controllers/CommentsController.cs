f8e1b60d084303f9a5f92fed9d0911b8a5cf9f05
KeaneDT <S10202630@connect.np.edu.sg>
2021-11-15 16:35:54 +0800

    Minor Changes to Views (Need to add Create Comment View)

--- Controllers/CommentsController.cs
+++ Controllers/CommentsController.cs
@@ -1,18 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using PFD_SaveTheEnvironment.DAL;
 using PFD_SaveTheEnvironment.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PFD_SaveTheEnvironment.Controllers
 {
     public class CommentsController : Controller
     {
+        private UsersDAL userContext = new UsersDAL();
         public async Task<ActionResult> Index()
         {
+            if ((HttpContext.Session.GetString("Role") == null) || (HttpContext.Session.GetString("Role") != "User"))
+            {
+                ViewData["Role"] = null;
+            }
+            else
+            {
+                ViewData["Role"] = "User";
+            }
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://commentsapi20211115072014.azurewebsites.net/");
             HttpResponseMessage response = await client.GetAsync("/api/comments");
@@ -25,6 +38,32 @@ namespace PFD_SaveTheEnvironment.Controllers
             else
             {
                 return View(new List<Comments>());
+            }
+        }
+        public async Task<ActionResult> Create(IFormCollection collection)
+        {
+            string commentText = collection["item.Comment"];
+            // Transfer data read to a vote object
+            Comments c = new Comments();
+            c.Id = 0;
+            c.UserName = userContext.GetUserName(HttpContext.Session.GetString("LoginID"));
+            c.Comment = commentText;
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new
+            Uri("https://commentsapi20211115072014.azurewebsites.net/");
+            string json = JsonConvert.SerializeObject(c);
+            StringContent commentscontent = new StringContent(json, UnicodeEncoding.UTF8,
+            "application/json");
+            HttpResponseMessage response = await client.PostAsync("/api/comments",
+            commentscontent);
+            if (response.StatusCode == HttpStatusCode.Created)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["Message"] = "Failed to add comment!";
+                return RedirectToAction("Index", "Comments");
             }
         }
     }
