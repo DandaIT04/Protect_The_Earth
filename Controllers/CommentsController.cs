bf6876706009ee9c4785d7cff6ec82448c8a81d4
KeaneDT <S10202630@connect.np.edu.sg>
2021-11-15 10:00:00 +0800

    Added Comments Index to view comments from azure web api

---
+++ Controllers/CommentsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PFD_SaveTheEnvironment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PFD_SaveTheEnvironment.Controllers
+{
+    public class CommentsController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://commentsapi20211115072014.azurewebsites.net/");
+            HttpResponseMessage response = await client.GetAsync("/api/comments");
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                List<Comments> commentsList = JsonConvert.DeserializeObject<List<Comments>>(data);
+                return View(commentsList);
+            }
+            else
+            {
+                return View(new List<Comments>());
+            }
+        }
+    }
+}
