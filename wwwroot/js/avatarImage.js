5ed6d3565cccad9a4105014f214e523b2f4aa304
KeaneDT <S10202630@connect.np.edu.sg>
2021-11-18 15:25:48 +0800

    Changed Home View

---
+++ wwwroot/js/avatarImage.js
@@ -0,0 +1,13 @@
+﻿function httpGetAsync(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+}
+
+var url = "https://avatars.abstractapi.com/v1/?api_key=1cd41182df6145838077a068b9036f92&name=Claire Florentz&background_color=00adef&image_size=165"
+
+httpGetAsync(url)
